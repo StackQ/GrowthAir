8d54b5f669ff880cb38aad8ac14c959ed15c585f
PsychoUranium <83650379+PsychoUranium@users.noreply.github.com>
2023-11-22 18:38:19 +0000

    bug fix

--- dags/producer.py
+++ dags/producer.py
@@ -6,7 +6,7 @@ from datetime import date, datetime
 my_file = Dataset("/tmp/my_file.txt")
 my_file_2 = Dataset("/tmp/my_file_2.txt")
 
-with DAG(dag_id="producer", scheduler="@daily", start_date=datetime(2023,11,20), catchup=False):
+with DAG(dag_id="producer", schedule="@daily", start_date=datetime(2023,11,20), catchup=False):
 
     @task(outlets=[my_file])
     def update_dataset():
